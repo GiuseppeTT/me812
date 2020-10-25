76f867bc67394ce89a1507117778f380d0d6d937
GiuseppeTT <giu.tinti@gmail.com>
2020-10-25 13:42:54 -0300

    Add project structure

---
+++ main.R
@@ -0,0 +1,33 @@
+# Load packages ----------------------------------------------------------------
+library(tidyverse)
+library(drake)
+
+
+
+# Define helper functions for main.R -------------------------------------------
+source_directory <- function(
+    path,
+    ...
+) {
+    path %>%
+        fs::dir_ls(type = "file", glob = "*.R", ...) %>%
+        walk(source)
+}
+
+
+
+# Load sub plans and their functions -------------------------------------------
+source_directory("functions/")
+source_directory("subplans/")
+
+
+
+# Define main plan -------------------------------------------------------------
+plan <- bind_plans(
+    # Empty
+)
+
+
+
+# Make plan --------------------------------------------------------------------
+make(plan)

c5197b4f72831b7aa93d49a211d78a5150037249
Giuseppe Tinti Tomio <67562667+GiuseppeTT@users.noreply.github.com>
2020-12-08 20:10:48 -0300

    Add last plots from Pedro (#17)

--- functions/exploratory_analysis/my_plots.R
+++ functions/exploratory_analysis/my_plots.R
@@ -20,3 +20,69 @@ my_mean_acceptability_plot <- function(summary_table){
              return()
     }
 
+
+
+
+my_boxplot_verbform <- function(complete_data){
+    my_summary_table(complete_data) %>%
+        ggplot(aes(x = verb_form, y = mean_acceptability, color = country)) +
+        geom_boxplot() +
+        scale_y_continuous(limits = c(0, 5), expand = c(0, 0)) +
+        coord_flip() +
+        labs(
+            x = "Forma Verbal",
+            y = "Aceitabilidade",
+            color = "País"
+        ) +
+        theme_bw()  %>%
+        return()
+}
+
+my_boxplot_order <- function(complete_data){
+    my_summary_table_order(complete_data) %>%
+        ggplot(aes(x = order, y = mean_acceptability)) +
+        geom_boxplot() +
+        scale_y_continuous(limits = c(0, 5), expand = c(0, 0)) +
+        coord_flip() +
+        labs(
+            x = "Ordem",
+            y = "Aceitabilidade"
+        ) +
+        theme_bw() %>%
+        return()
+}
+
+
+my_barplot_age<- function(complete_data){
+    complete_data %>%
+        distinct(id,.keep_all = T) %>%
+        group_by(age) %>%
+        count() %>%
+        ggplot(aes(x = age, y = n)) +
+        geom_bar(stat = "identity") +
+        geom_text(aes(label = n ), vjust=-0.25, color = "black")  +
+        labs(
+            y = "Frequência",
+            x = "Faixa Etária"
+        ) +
+        theme_bw() %>%
+        return()
+}
+
+my_barplot_gender <- function(complete_data){
+    complete_data %>%
+        distinct(id,.keep_all = T) %>%
+        group_by(gender, country) %>%
+        mutate(n = n()) %>%
+        ggplot(aes(x = gender, y = n)) +
+        geom_bar(stat = "identity",position = 'dodge') +
+        facet_grid(cols = vars(country)) +
+        geom_text(aes(label = n ), vjust= -0.25, color = "black") +
+        labs(
+            y = "Frequência",
+            x = "Gênero"
+        ) +
+        theme_bw() %>%
+        return()
+}
+
